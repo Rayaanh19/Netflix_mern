65e497c940aeca056228998945090ded63e12eec
rayaanh <rayaanh19@gmail.com>
2024-09-11 15:04:59 +0530

    Netflix MERN

---
+++ backend/routes/auth.route.js
@@ -0,0 +1,15 @@
+import express from "express";
+import {signup, login, logout, authCheck} from "../controllers/auth.controller.js";
+
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
+
+router.get("/authCheck", authCheck);
+
+export default router;
+
+
+//EyjSGZYmEp1mwCSI
